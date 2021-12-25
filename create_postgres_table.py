8323b7ce1a5e944bd5fb7ea0e06ec8d615ea48b8
Engineer-Hohenzollern <95528781+Engineer-Hohenzollern@users.noreply.github.com>
2021-12-25 14:15:31 +0530

    Create create_postgres_table.py

---
+++ create_postgres_table.py
@@ -0,0 +1,34 @@
+# we are using SqlAlchemy ORM object-relational map) to create a Postgres table using Python classes
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.update(
+
+    SECRET_KEY='Antz.Ai',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:richy@localhost/password_project_db',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
+
+db = SQLAlchemy(app)
+
+
+class user (db.Model):
+    __tablename__ = 'user'
+
+    userid = db.Column(db.String(80), primary_key=True)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, userid, password):
+        self.userid = userid
+        self.password = password
+
+    def __repr__(self):
+        return 'This is your userid  {}, your password  is {}'.format(self.userid, self.password)
+
+
+if __name__ == '__main__':          # If the table does not exist in the database, it is created.
+    db.create_all()
+    app.run(debug=True)
